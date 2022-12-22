38c6c9bcd3e324484972db50b4b4707fc4ca6357
pratik149 <pratikrane149@gmail.com>
2022-12-22 07:30:47 +0530

    style: :art: configure prettier and eslint and execute first lint

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
   </main>
 </template>
