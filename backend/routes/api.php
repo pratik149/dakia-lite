1fc989f9e9d70154b680f8bd659f69d5330f447e
pratik149 <pratikrane149@gmail.com>
2022-12-26 03:52:24 +0530

    feat: :truck: create crud api routes for fields and subscribers

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\SubscriberController;
+use App\Http\Controllers\FieldController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +17,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('subscribers', SubscriberController::class);
+Route::apiResource('fields', FieldController::class);
