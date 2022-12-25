a1067dd19c28fac9613763e5c520835109516191
pratik149 <pratikrane149@gmail.com>
2022-12-26 04:04:24 +0530

    feat: :truck: create frontend routes for subscribers and subscriber details page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: "/",
@@ -12,10 +12,12 @@ const router = createRouter({
     {
       path: "/subscribers",
       name: "subscribers",
-      // route level code-splitting
-      // this generates a separate chunk (Subscribers.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import("../views/SubscribersView.vue"),
+    },
+    {
+      path: "/subscribers/:id",
+      name: "subscriber-details",
+      component: () => import("../views/SubscriberView.vue"),
     },
   ],
 });
