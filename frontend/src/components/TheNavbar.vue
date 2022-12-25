3ce0c837b85ae271cd8d0fd3f8d7a25c73ccc964
pratik149 <pratikrane149@gmail.com>
2022-12-26 03:57:31 +0530

    refactor: :lipstick: update navbar and home view

--- frontend/src/components/TheNavbar.vue
+++ frontend/src/components/TheNavbar.vue
@@ -1,12 +1,12 @@
 <script setup>
 import { RouterLink } from "vue-router";
-import IconMailerLite from "./icons/IconMailerLite.vue";
+import IconDakiaLite from "./icons/IconDakiaLite.vue";
 </script>
 
 <template>
-  <header class="flex shrink-0 justify-between bg-white p-4 mx-auto max-w-8xl">
+  <header class="mx-auto flex w-full max-w-8xl flex-initial basis-auto justify-between bg-white px-4 py-6">
     <RouterLink class="text-2xl font-black tracking-tight" to="/">
-      <IconMailerLite />
+      <IconDakiaLite />
     </RouterLink>
     <nav>
       <RouterLink class="rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-100" to="/">Home</RouterLink>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,31 +3,27 @@ import { RouterLink } from "vue-router";
 </script>
 
 <template>
-  <main class="flex h-screen flex-col items-center justify-around">
+  <main class="mx-auto flex h-full flex-auto flex-col justify-between py-16">
     <div class="max-w-md text-center">
-      <h1 class="text-2xl font-semibold text-gray-700">Welcome to MailerLite</h1>
+      <h1 class="text-2xl font-semibold text-gray-700">Welcome to <span class="text-green-500"> DakiaLite </span></h1>
       <p class="mt-2 text-gray-500">
         Digital marketing tools to grow your audience faster and drive revenue smarter. Backed by 24/7 award-winning
         support.
       </p>
     </div>
 
-    <div>
-      <RouterLink
-        to="/subscribers"
-        class="rounded-lg bg-blue-100 px-4 py-2 text-center text-3xl font-extrabold text-gray-800 transition duration-150 ease-out hover:bg-blue-600 hover:text-white"
-      >
-        Explore
-      </RouterLink>
-    </div>
+    <RouterLink
+      to="/subscribers"
+      class="mx-auto block max-w-max rounded-lg bg-green-100 px-4 py-2 text-center text-3xl font-extrabold text-gray-800 transition duration-150 ease-out hover:bg-green-600 hover:text-white"
+    >
+      Explore
+    </RouterLink>
 
-    <div>
-      <p class="text-center text-gray-600">
-        Made with 💚 by
-        <a href="https://github.com/pratik149/" class="text-blue-500 hover:text-blue-600" target="_blank">
-          @pratik149
-        </a>
-      </p>
-    </div>
+    <p class="text-center text-gray-600">
+      Made with 💚 by
+      <a href="https://github.com/pratik149/" class="text-green-500 hover:text-green-600" target="_blank">
+        @pratik149
+      </a>
+    </p>
   </main>
 </template>
