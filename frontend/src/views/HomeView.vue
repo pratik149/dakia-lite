06523d93b5f3740647ce9ff81c737ecf81a01d90
pratik149 <pratikrane149@gmail.com>
2022-12-26 12:57:51 +0530

    style: :lipstick: minor spacing update in home page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -5,19 +5,20 @@ import { RouterLink } from "vue-router";
 <template>
   <main class="mx-auto flex h-full flex-auto flex-col justify-between py-16">
     <div class="max-w-md text-center">
-      <h1 class="text-2xl font-semibold text-gray-700">Welcome to <span class="text-green-500"> DakiaLite </span></h1>
-      <p class="mt-2 text-gray-500">
+      <h1 class="mb-4 text-2xl font-semibold text-gray-700">
+        Welcome to <span class="text-green-500"> DakiaLite </span>
+      </h1>
+      <p class="mb-6 text-gray-500">
         Dakia (Hindi word for postman/mailer, pronounced /daa-ki-yaa/) is a tiny feature clone of MailerLite built using
         Laravel 9, Vue3 and TailwindCSS.
       </p>
+      <RouterLink
+        to="/subscribers"
+        class="mx-auto block max-w-max rounded-lg bg-green-100 px-4 py-2 text-center text-3xl font-extrabold text-gray-800 transition duration-150 ease-out hover:bg-green-600 hover:text-white"
+      >
+        Explore
+      </RouterLink>
     </div>
-
-    <RouterLink
-      to="/subscribers"
-      class="mx-auto block max-w-max rounded-lg bg-green-100 px-4 py-2 text-center text-3xl font-extrabold text-gray-800 transition duration-150 ease-out hover:bg-green-600 hover:text-white"
-    >
-      Explore
-    </RouterLink>
 
     <p class="text-center text-gray-600">
       Made with 💚 by
